b9d89d488d119b602da49236e093c4ae739c7d6d
Taha Afzal <tahaafzal5@hotmail.com>
2024-03-02 10:02:13 -0600

    Change progress output from rsync

--- backup-macBook/backupMacBookToFlashDrive.sh
+++ backup-macBook/backupMacBookToFlashDrive.sh
@@ -39,7 +39,7 @@ perform_backup() {
 
     for sourceDir in "${sourceDirs[@]}"; do
         echo "Backing up $baseDir$sourceDir..."
-        rsync -azvPh --delete --exclude 'TV' --exclude '*.imovielibrary' --exclude '*.photoslibrary' \
+        rsync -azh --info=progress2 --delete --exclude 'TV' --exclude '*.imovielibrary' --exclude '*.photoslibrary' \
               --exclude '*.theater' "$baseDir$sourceDir" "$destination"
     done
 }
